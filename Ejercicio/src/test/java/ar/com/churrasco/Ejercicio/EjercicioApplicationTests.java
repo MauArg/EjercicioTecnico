d408ff8ddc8c0309362a9d2b4cdf133c0dfc670b
MauArg <maulp_gnt@hotmail.com>
2020-10-29 22:47:08 -0300

    Primer commit desde Eclipse

---
+++ Ejercicio/src/test/java/ar/com/churrasco/Ejercicio/EjercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.churrasco.Ejercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
